c27c139bf1f46601a0950303f77d326054882dae
Bhavesh <bhavesh@grepruby.com>
2013-07-26 14:08:19 +0530

    add validation in User Model

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,13 +12,13 @@ class User < ActiveRecord::Base
   has_many :task_comments
   has_many :feedbacks
   
-#  validates :first_name, :last_name, :address, :mobile_no, presence: true
-#  validates :first_name, length: { minimum: 2}
-#  validates :last_name, length: { minimum: 2}
-#  validates_each :first_name, :last_name do |record, attr, value| 
-#    record.errors.add( attr, 'Must start with upper case') if value =~ /\A[a-z]/
-#  end
-#  validates :mobile_no,numericality: { only_integers: true }, length: { is: 10}
+  validates :first_name, :last_name, :address, :mobile_no, presence: true
+  validates :first_name, length: { minimum: 2}
+  validates :last_name, length: { minimum: 2}
+  validates_each :first_name, :last_name do |record, attr, value| 
+    record.errors.add( attr, 'Must start with upper case') if value =~ /\A[a-z]/
+  end
+  validates :mobile_no,numericality: { only_integers: true }, length: { is: 10}
   
   scope :get_user, lambda { |user_id| where(:id => user_id) }
   
