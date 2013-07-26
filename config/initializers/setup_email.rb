7cb59526382782b7c89bc4869852903385be27f1
Bhavesh <bhavesh@grepruby.com>
2013-07-26 16:01:08 +0530

    mail setup

---
+++ config/initializers/setup_email.rb
@@ -0,0 +1,8 @@
+ActionMailer::Base.smtp_settings = {
+ :address              => "smtp.gmail.com",
+ :port                 => 587,
+ :user_name            => "vrishi18@gmail.com",
+ :password             => "@khushi143",
+ :authentication       => "plain",
+ :enable_starttls_auto => true
+}
