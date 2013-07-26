ce5f3c3b41868da1f984ced113e7ad1feee8171c
Bhavesh <bhavesh@grepruby.com>
2013-07-26 13:37:33 +0530

    remove validations from User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,20 +6,19 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
   
   has_and_belongs_to_many :teams, join_table: 'team_members'
   has_many :tasks
   has_many :task_comments
   has_many :feedbacks
   
-  validates :first_name, :last_name, :address, :mobile_no, presence: true
-  validates :first_name, length: { minimum: 2}
-  validates :last_name, length: { minimum: 2}
-  validates_each :first_name, :last_name do |record, attr, value| 
-    record.errors.add( attr, 'Must start with upper case') if value =~ /\A[a-z]/
-  end
-  validates :mobile_no,numericality: { only_integers: true }, length: { is: 10}
+#  validates :first_name, :last_name, :address, :mobile_no, presence: true
+#  validates :first_name, length: { minimum: 2}
+#  validates :last_name, length: { minimum: 2}
+#  validates_each :first_name, :last_name do |record, attr, value| 
+#    record.errors.add( attr, 'Must start with upper case') if value =~ /\A[a-z]/
+#  end
+#  validates :mobile_no,numericality: { only_integers: true }, length: { is: 10}
   
   scope :get_user, lambda { |user_id| where(:id => user_id) }
   
