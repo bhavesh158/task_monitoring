6a230d3abdae7b0d177d9a9179f75d7d39262e66
Bhavesh <bhavesh@grepruby.com>
2013-07-26 17:45:53 +0530

    Code changed in task_comment_mailer.rb

--- app/mailers/task_comment_mailer.rb
+++ app/mailers/task_comment_mailer.rb
@@ -3,18 +3,14 @@ class TaskCommentMailer < ActionMailer::Base
   
   def new_task_comment_notification(task_comment, current_user_id)
     @current_user_type = TeamMember.where(user_id: current_user_id).first.user_type
+    @current_user = User.where(id: current_user_id).first
+    @task_comment = task_comment
+    @task = Task.where(id: @task_comment.task_id).first
+    @task_team = Team.where(id: @task.team_id).first
     if @current_user_type == "Team Lead"
-      @current_user = User.where(id: current_user_id).first
-      @task_comment = task_comment
-      @task = Task.where(id: @task_comment.task_id).first
-      @task_team = Team.where(@task.team_id).first
       @user = User.where(id: Task.find(@task_comment.task_id).user_id).first
       mail(to: @user.email, subject: 'New task comment for you')
     else
-      @current_user = User.where(id: current_user_id).first
-      @task_comment = task_comment
-      @task = Task.where(id: @task_comment.task_id).first
-      @task_team = Team.where(@task.team_id).first
       @team_lead = @task_team.team_members.where(user_type: "Team Lead").first
       @user = User.where(id: @team_lead.user_id).first
       mail(to: @user.email, subject: 'New task comment for you')
