f7db2bf330acf850ae9dfcb600b489e7c2a8f079
Bhavesh <bhavesh@grepruby.com>
2013-07-26 17:16:09 +0530

    Change in create action of tasks_comments_controller.rb

--- app/mailers/task_comment_mailer.rb
+++ app/mailers/task_comment_mailer.rb
@@ -1,17 +1,17 @@
 class TaskCommentMailer < ActionMailer::Base
   default from: "support@grepruby.com"
   
-  def new_task_comment_notification(task_comment, current_user)
-    @current_user_type = TeamMember.where(user_id: current_user).first.user_type
+  def new_task_comment_notification(task_comment, current_user_id)
+    @current_user_type = TeamMember.where(user_id: current_user_id).first.user_type
     if @current_user_type == "Team Lead"
-      @current_user = User.where(id: current_user).first
+      @current_user = User.where(id: current_user_id).first
       @task_comment = task_comment
       @task = Task.where(id: @task_comment.task_id).first
       @task_team = Team.where(@task.team_id).first
       @user = User.where(id: Task.find(@task_comment.task_id).user_id).first
       mail(to: @user.email, subject: 'New task comment for you')
     else
-      @current_user = User.where(id: current_user).first
+      @current_user = User.where(id: current_user_id).first
       @task_comment = task_comment
       @task = Task.where(id: @task_comment.task_id).first
       @task_team = Team.where(@task.team_id).first
